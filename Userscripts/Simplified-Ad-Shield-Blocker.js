1bee0c5f54d32f4bae923b638393a96ba446a13c
finallycometolife <70871779+finallycometolife@users.noreply.github.com>
2024-11-06 08:21:36 +0900

    문구 추가

--- Userscripts/Simplified-Ad-Shield-Blocker.js
+++ Userscripts/Simplified-Ad-Shield-Blocker.js
@@ -1,7 +1,9 @@
 // ==UserScript==
-// @name         Simplified Ad-Shield Blocker
-// @namespace    http://tampermonkey.net/
-// @version      1.2
+// @name         Simplified Anti AD Shield 
+// @namespace    https://finallycometolife.github.io
+// @updateURL    https://finallycometolife.github.io/honeysaeromFilter/Userscripts/Simplified-Ad-Shield-Blocker.js
+// @downloadURL  https://finallycometolife.github.io/honeysaeromFilter/Userscripts/Simplified-Ad-Shield-Blocker.js
+// @version      1.3
 // @description  Block AdShield script execution on websites
 // @match        *://*/*
 // @grant        none
@@ -12,7 +14,7 @@
 
     // AdShield 변수 및 함수 이름 차단
     ['adshield', 'detectAdblock', 'blockDetected', 'adblock', 'blockAdBlock', 'detectAds'].forEach(name => {
-        let temp;  // let을 이용해 변수 선언
+        let temp;  //
         Object.defineProperty(window, name, {
             configurable: false,
             writable: false,
